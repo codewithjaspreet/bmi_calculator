334303ba34fa4e94142b4005ad66c14aa0a3b833
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-03-27 15:00:08 +0530

    added more constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,10 @@ const kBottomContainerHeight = 80.0;
 const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomContainerColor = Color(0xFFEB1555);
-
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
